934a16065fab867fbd0ba3afe20589386911ff5b
Joel Otero <joel.otero@yahoo.com>
2020-12-09 20:59:08 -0500

    Changed h3 for h2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ class Home extends Component {
         return (
             <div>
             <h1>Welcome to Natural store</h1>
-            <h3>Chech our amazing catalog!!</h3>
+            <h2>Chech our amazing catalog!!</h2>
             </div>
         );
     
