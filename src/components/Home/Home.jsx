c254d2ecf85a2cd3ee56fdbd537809bbbb13fbd3
Joel Otero <jolo@MacBook-Air.lan1>
2020-12-09 20:42:19 -0500

    Fix non-existing bug

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Home extends Component {
     render() { 
         return (
+            <div>
             <h1>Welcome to Natural store</h1>
+            <h3>Chech our amazing catalog!!</h3>
+            </div>
         );
     
     }
