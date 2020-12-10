b37839f8231463888609b880316e0a30a29c96ea
Joel Otero <jolo@MacBook-Air.lan1>
2020-12-09 20:33:35 -0500

    initial commit - display catalog working

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() { 
+        return (
+            <h1>Welcome to Natural store</h1>
+        );
+    
+    }
+}
+ 
+export default Home;
